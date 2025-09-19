7b2acd2d11b6ce7c91a2a7f68854303d86ac288d
asifsha <asif-sharif@hotmail.com>
2025-09-19 22:55:37 +0800

    irsa role added in infra

--- cdk/lib/infra-stack.js
+++ cdk/lib/infra-stack.js
@@ -3,124 +3,122 @@ const eks = require('aws-cdk-lib/aws-eks');
 const ec2 = require('aws-cdk-lib/aws-ec2');
 const ecr = require('aws-cdk-lib/aws-ecr');
 const iam = require('aws-cdk-lib/aws-iam');
-const s3 = require('aws-cdk-lib/aws-s3');
 const dynamodb = require('aws-cdk-lib/aws-dynamodb');
 const { Construct } = require('constructs');
 
 class InfraStack extends cdk.Stack {
-    constructor(scope, id, props) {
-        super(scope, id, props);
+  constructor(scope, id, props) {
+    super(scope, id, props);
 
+    // VPC with subnet tagging for EKS load balancers
+    const vpc = new ec2.Vpc(this, 'Vpc', {
+      maxAzs: 2,
+      subnetConfiguration: [
+        { cidrMask: 24, name: 'PublicSubnet', subnetType: ec2.SubnetType.PUBLIC },
+        { cidrMask: 24, name: 'PrivateSubnet', subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS }
+      ]
+    });
 
-        // VPC with subnet tagging for EKS load balancers
-        const vpc = new ec2.Vpc(this, 'Vpc', {
-            maxAzs: 2,
-            subnetConfiguration: [
-                {
-                    cidrMask: 24,
-                    name: 'PublicSubnet',
-                    subnetType: ec2.SubnetType.PUBLIC,
-                },
-                {
-                    cidrMask: 24,
-                    name: 'PrivateSubnet',
-                    subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
-                }
-            ]
-        });
+    // DynamoDB
+    const table = new dynamodb.Table(this, 'ItemsTable', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
 
-        // Tag subnets for ELB usage
-        vpc.publicSubnets.forEach((subnet, i) => {
-            cdk.Tags.of(subnet).add('kubernetes.io/role/elb', '1');
-            cdk.Tags.of(subnet).add(`kubernetes.io/cluster/${cluster.clusterName}`, 'shared');
-        });
+    // ECR repo for the app image
+    const repo = new ecr.Repository(this, 'EcrRepo', {
+      repositoryName: 'node-api'
+    });
 
-        vpc.privateSubnets.forEach((subnet, i) => {
-            cdk.Tags.of(subnet).add('kubernetes.io/role/internal-elb', '1');
-            cdk.Tags.of(subnet).add(`kubernetes.io/cluster/${cluster.clusterName}`, 'shared');
-        });
+    // EKS Cluster
+    const cluster = new eks.Cluster(this, 'EksCluster', {
+      vpc,
+      version: eks.KubernetesVersion.V1_30,
+      defaultCapacity: 2
+    });
 
-        // ECR repo for the app image
-        const repo = new ecr.Repository(this, 'EcrRepo', {
-            repositoryName: 'node-api'
-        });
+    // Tag subnets for ELB usage
+    vpc.publicSubnets.forEach((subnet) => {
+      cdk.Tags.of(subnet).add('kubernetes.io/role/elb', '1');
+      cdk.Tags.of(subnet).add(`kubernetes.io/cluster/${cluster.clusterName}`, 'shared');
+    });
+    vpc.privateSubnets.forEach((subnet) => {
+      cdk.Tags.of(subnet).add('kubernetes.io/role/internal-elb', '1');
+      cdk.Tags.of(subnet).add(`kubernetes.io/cluster/${cluster.clusterName}`, 'shared');
+    });
 
-        // DynamoDB
-        const table = new dynamodb.Table(this, 'ItemsTable', {
-            partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
-            billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-            removalPolicy: cdk.RemovalPolicy.DESTROY
-        });
+    // IRSA Role for node-api
+    const nodeApiSa = cluster.addServiceAccount('NodeApiServiceAccount', {
+      name: 'node-api-sa',
+      namespace: 'default'
+    });
 
-        // EKS Cluster
-        const cluster = new eks.Cluster(this, 'EksCluster', {
-            vpc,
-            defaultCapacity: 2,
-            version: eks.KubernetesVersion.V1_30,
-        });
+    nodeApiSa.role.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonDynamoDBFullAccess')
+    );
+    nodeApiSa.role.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonCognitoReadOnly')
+    );
 
-        cluster.awsAuth.addMastersRole(
-            iam.Role.fromRoleArn(this, 'GitHubActionsRole',
-                'arn:aws:iam::412381746256:role/githubAccessECRECSRole'
-            )
-        );
+    // Grant nodes access to pull from ECR
+    repo.grantPull(cluster.defaultNodegroup?.role || cluster.defaultCapacity?.role);
+    table.grantReadWriteData(cluster.defaultNodegroup?.role || cluster.defaultCapacity?.role);
 
-        cluster.awsAuth.addMastersRole(
-            iam.Role.fromRoleArn(this, 'GitHubActionsRole',
-                'arn:aws:iam::412381746256:user/devcli'
-            )
-        );
-
-        // Grant nodes access to read/write Dynamo and pull from ECR
-        repo.grantPull(cluster.defaultNodegroup?.role || cluster.defaultCapacity?.role);
-        table.grantReadWriteData(cluster.defaultNodegroup?.role || cluster.defaultCapacity?.role);
-
-        // Apply k8s manifests (deployment + service). Replace image later in CI when pushing to ECR.
-        const manifestDeployment = {
-            apiVersion: 'apps/v1',
-            kind: 'Deployment',
-            metadata: { name: 'node-api' },
-            spec: {
-                replicas: 2,
-                selector: { matchLabels: { app: 'node-api' } },
-                template: {
-                    metadata: { labels: { app: 'node-api' } },
-                    spec: {
-                        containers: [{ name: 'node-api', image: repo.repositoryUriForTag('latest'), ports: [{ containerPort: 3000 }], env: [{ name: 'TABLE_NAME', value: table.tableName }, { name: 'AWS_REGION', value: this.region }] }]
-                    }
-                }
-            }
-        };
-
-        cluster.addManifest('AppDeployment', manifestDeployment);
-
-        const svc = {
-            apiVersion: 'v1',
-            kind: 'Service',
-            metadata: { name: 'node-api-svc' },
-            spec: {
-                type: 'LoadBalancer',
-                selector: { app: 'node-api' },
-                ports: [
-                    { port: 80, targetPort: 3000 } // Public port 80 -> container port 3000
+    // Apply k8s manifests (deployment + service)
+    const manifestDeployment = {
+      apiVersion: 'apps/v1',
+      kind: 'Deployment',
+      metadata: { name: 'node-api' },
+      spec: {
+        replicas: 2,
+        selector: { matchLabels: { app: 'node-api' } },
+        template: {
+          metadata: { labels: { app: 'node-api' } },
+          spec: {
+            serviceAccountName: nodeApiSa.serviceAccountName, // bind IRSA
+            containers: [
+              {
+                name: 'node-api',
+                image: repo.repositoryUriForTag('latest'),
+                ports: [{ containerPort: 3000 }],
+                env: [
+                  { name: 'TABLE_NAME', value: table.tableName },
+                  { name: 'AWS_REGION', value: this.region }
                 ]
-            }
-        };
+              }
+            ]
+          }
+        }
+      }
+    };
 
-        const svcManifest = cluster.addManifest('AppService', svc);
+    cluster.addManifest('AppDeployment', manifestDeployment);
 
-        new cdk.CfnOutput(this, 'EcrRepoUri', { value: repo.repositoryUri });
-        new cdk.CfnOutput(this, 'ClusterName', { value: cluster.clusterName });
-        new cdk.CfnOutput(this, 'TableName', { value: table.tableName });
+    const svc = {
+      apiVersion: 'v1',
+      kind: 'Service',
+      metadata: { name: 'node-api-svc' },
+      spec: {
+        type: 'LoadBalancer',
+        selector: { app: 'node-api' },
+        ports: [{ port: 80, targetPort: 3000 }]
+      }
+    };
 
-        // Output LoadBalancer DNS once available
-        new cdk.CfnOutput(this, 'ServiceEndpoint', {
-            value: cdk.Fn.join('', [
-                'http://',
-                svcManifest.getAtt('status.loadBalancer.ingress.0.hostname')
-            ])
-        });
-    }
+    const svcManifest = cluster.addManifest('AppService', svc);
+
+    // Outputs
+    new cdk.CfnOutput(this, 'EcrRepoUri', { value: repo.repositoryUri });
+    new cdk.CfnOutput(this, 'ClusterName', { value: cluster.clusterName });
+    new cdk.CfnOutput(this, 'TableName', { value: table.tableName });
+    new cdk.CfnOutput(this, 'ServiceEndpoint', {
+      value: cdk.Fn.join('', [
+        'http://',
+        svcManifest.getAtt('status.loadBalancer.ingress.0.hostname')
+      ])
+    });
+  }
 }
 
 module.exports = { InfraStack };
